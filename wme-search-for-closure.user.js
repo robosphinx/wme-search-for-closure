4e762289fe691988c36a09240d80f02ab171ed0d
Robosphinx <robosphinx@gmail.com>
2025-10-15 20:34:32 -0400

    Add check for existing icon
    - Add additional unique class name to icon
    - Check for existence of that unique class name before adding icon

--- wme-search-for-closure.user.js
+++ wme-search-for-closure.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Search for Closure
 // @namespace    https://github.com/WazeDev/wme-search-for-closure
-// @version      0.0.2
+// @version      0.0.3
 // @description  Adds a search icon/button to find closures on a selected segment using Google Search.
 // @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
 // @match        https://beta.waze.com/*editor*
@@ -12,6 +12,8 @@
 // @contributionURL https://github.com/WazeDev/Thank-The-Authors
 // @license      MIT
 // @grant        none
+// @downloadURL https://update.greasyfork.org/scripts/541362/WME%20Search%20for%20Closure.user.js
+// @updateURL https://update.greasyfork.org/scripts/541362/WME%20Search%20for%20Closure.meta.js
 // ==/UserScript==
 
 (function() {
@@ -24,13 +26,13 @@
     let searchIconCreated = false;
 
     function delay(time = 1000) {
-  if (time <= 0) {
-    return Promise.resolve(); // No delay needed
-  }
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time);
-  });
-}
+        if (time <= 0) {
+            return Promise.resolve(); // No delay needed
+        }
+        return new Promise(function (resolve) {
+            setTimeout(resolve, time);
+        });
+    }
 
     function waitForElm(selector, doc) {
         return new Promise(resolve => {
@@ -72,27 +74,34 @@
         console.log("WME Search for Closure: Initialized!");
     }
     async function createSearchIcon() {
-        console.log("WME Search for Closure: Adding search icon...");
-        selectionHeader = document.getElementsByClassName("segment sidebar-column")[0].children[0];
-        let iconDiv = document.createElement("div");
-        iconDiv.className = "icon";
-        iconDiv.id = "wsfc-search-icon-div";
-        let slot = document.createElement("slot");
-        slot.name = "search-icon";
-        slot.id = "wsfc-search-icon-slot";
-        let icon = document.createElement("i");
-        icon.slot = "search-icon";
-        icon.className = "w-icon w-icon-search";
-        icon.id = "wsfc-search-icon-slot";
-        selectionHeader.appendChild(icon);
-        link = document.createElement("a");
-        link.style = "margin-left: auto;";
-        link.target = "_blank";
-        iconDiv.appendChild(slot);
-        link.appendChild(iconDiv);
-        waitForElm('.wz-section-header-bottom-border', selectionHeader.shadowRoot).then((elm) => {
-            selectionHeader.shadowRoot.children[0].appendChild(link);
-        });
+        if (document.getElementsByClassName("wsfc-icon").length === 0)
+        {
+            console.log("WME Search for Closure: Adding search icon...");
+            selectionHeader = document.getElementsByClassName("segment sidebar-column")[0].children[0];
+            let iconDiv = document.createElement("div");
+            iconDiv.className = "icon";
+            iconDiv.id = "wsfc-search-icon-div";
+            let slot = document.createElement("slot");
+            slot.name = "search-icon";
+            slot.id = "wsfc-search-icon-slot";
+            let icon = document.createElement("i");
+            icon.slot = "search-icon";
+            icon.className = "w-icon w-icon-search wsfc-icon";
+            icon.id = "wsfc-search-icon-slot";
+            selectionHeader.appendChild(icon);
+            link = document.createElement("a");
+            link.style = "margin-left: auto;";
+            link.target = "_blank";
+            iconDiv.appendChild(slot);
+            link.appendChild(iconDiv);
+            waitForElm('.wz-section-header-bottom-border', selectionHeader.shadowRoot).then((elm) => {
+                selectionHeader.shadowRoot.children[0].appendChild(link);
+            });
+        }
+        // else
+        // {
+        //     console.log("WME Search for Closure: Search icon already existed.");
+        // }
     }
 
     async function onSelection() {
